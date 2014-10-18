76af04726a7f918e492e330c5c2cd24239a2cf1a
Andrey Yanov <andrey.cplusplus@gmail.com>
2014-10-18 13:58:45 +0300

    Added main file

---
+++ main.py
@@ -0,0 +1,21 @@
+def main():
+    name = input("Назовитесь: ")
+    try:
+        if len(name) == 0:
+            raise ValueError("Жаль, что вы не захотели представится")
+        print("Hello, {}".format(name))
+    except ValueError as e:
+        print(e)
+        exit()
+
+    try:
+        age = int(input("Возраст: "))
+        if age > 40:
+            print("Зарядку делаете??")
+        else:
+            print("Эх, молодой ещё!")
+    except ValueError as e:
+        print("Скрываете возраст? Ну и зря")
+        exit()
+
+main()
